3a88c469dd3b8380350e78986de1f9595eecfdea
Nikola-Kostov <niko_kostov@yahoo.com>
2021-02-05 19:10:54 +0100

    Initial commit

---
+++ src/main/java/com/project/boni/BoniApplication.java
@@ -0,0 +1,13 @@
+package com.project.boni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/boni/BoniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.boni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
